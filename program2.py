993766a05e1d4984198704c02d51a217fd87aabe
Abhishek <abhishekgorenka@gmail.com>
2024-11-11 14:03:37 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -13,7 +13,7 @@ def decode_message( message: str, pattern: str) -> bool:
 
         for i in range(1, m + 1):
                 for j in range(1, p + 1):
-                        if pattern[j - 1] == '*":
+                        if pattern[j - 1] == "*":
                                 dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
                         elif pattern[j - 1] == "?" or pattern[j - 1] == message[i - 1]:
                                 dp[i][j] = dp[i - 1][j - 1]
