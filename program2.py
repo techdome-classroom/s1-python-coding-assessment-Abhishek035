2fb86e4e54d2115ec9e5254d8d05da08eca15e13
Abhishek <abhishekgorenka@gmail.com>
2024-11-11 13:55:38 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,28 +1,6 @@
 def decode_message( s: str, p: str) -> bool:
 
-        if not grid or not grid[0]:
-                return 0
-
-        def dfs(r, c):
-                if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] != 'L':
-                        return
-
-                grid[r][c] = 'W'
-
-        dfs(r - 1, c)
-        dfs(r + 1, c)
-        dfs(r, c - 1)
-        dfs(r, c + 1)
-
-        island_count = 0
-
-        for r in range(len(grid)):
-                for c in range(len(grid[0])):
-                        if grid[r][c] == 'L':
-                        island_count += 1
-                        dfs(r, c)
-
-        return island_count
+        
 
 
   
