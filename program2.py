3bd08e58a80442c7e161e1e4ecf2274bc5aca816
Abhishek <abhishekgorenka@gmail.com>
2024-11-11 13:54:32 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,12 +1,12 @@
 def decode_message( s: str, p: str) -> bool:
 
         if not grid or not grid[0]:
-        return 0
+                return 0
 
-    def dfs(r, c):
+        def dfs(r, c):
         # If out of bounds or at water, return
         if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] != 'L':
-            return
+                return
         # Mark the current cell as visited
         grid[r][c] = 'W'
 
@@ -16,15 +16,15 @@ def decode_message( s: str, p: str) -> bool:
         dfs(r, c - 1)  # Left
         dfs(r, c + 1)  # Right
 
-    island_count = 0
+        island_count = 0
 
-    for r in range(len(grid)):
+        for r in range(len(grid)):
         for c in range(len(grid[0])):
-            if grid[r][c] == 'L':  # Found a new island
+                if grid[r][c] == 'L':  # Found a new island
                 island_count += 1
                 dfs(r, c)  # Mark all connected land as visited
 
-    return island_count
+        return island_count
 
 
   
